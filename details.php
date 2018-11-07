984fb1f87157587e889e15bb6d01fe3ed44eeb3e
sambrock <sambrocklehurst1998@gmail.com>
2018-11-07 21:29:35 +0000

    Added styles to design.php

--- details.php
+++ details.php
@@ -10,7 +10,7 @@ catch (PDOException $exception)
 
 $player_id=$_GET["id"];
 
-$stats_q = "SELECT *,
+$stats_q = "SELECT first_name, last_name, height, weight, date_of_birth, number, games,
 ROUND(SUM(minutes_played / games), 1) as MP,
 ROUND(SUM((three_point_fg+two_point_fg) / games),1) as FG,
 ROUND(SUM((three_point_fga+two_point_fga) / games),1) as FGA,
@@ -45,6 +45,16 @@ $pos_stmt->bindValue(":id", $player_id);
 
 $pos_stmt->execute();
 $player_pos=$pos_stmt->fetchAll();
+
+$awards_q ="SELECT name, year FROM awards
+INNER JOIN player_award ON awards.id = player_award.award_id INNER JOIN players ON player_award.player_id = players.id
+WHERE players.id = :id";
+$awards_stmt=$conn->prepare($awards_q);
+$awards_stmt->bindValue(":id", $player_id);
+
+$awards_stmt->execute();
+$awards=$awards_stmt->fetchAll();
+
 ?>
 
 <html>
@@ -54,61 +64,86 @@ $player_pos=$pos_stmt->fetchAll();
         <link href="style/style.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
-        <?php
-        echo "<h1>{$player["first_name"]} {$player["last_name"]}</h1>";
-        foreach($player_pos as $pos){
-            echo "<p>{$pos["name"]}</p>";
-        }
-        ?>
-        <table>
-            <thead>
-                <tr>
-                    <th><abbr title="Games Played">GP</abbr></th>
-                    <th><abbr title="Minuites Played">MP</abbr></th>
-                    <th><abbr title="Field Goals">FG</abbr></th>
-                    <th><abbr title="Field Goals Attempted">FGA</abbr></th>
-                    <th><abbr title="Field Goal Percentage">FG%</abbr></th>
-                    <th><abbr title="Three Point Field Goals">3P</abbr></th>
-                    <th><abbr title="Three Point Field Goal Attempts">3PA</abbr></th>
-                    <th><abbr title="Three Point Field Goal Percentage">3P%</abbr></th>
-                    <th><abbr title="Free Throws">FT</abbr></th>
-                    <th><abbr title="Free Throw Attempts">FTA</abbr></th>
-                    <th><abbr title="Free Throw Percentage">FT%</abbr></th>
-                    <th><abbr title="Offensive Rebounds">OREB</abbr></th>
-                    <th><abbr title="Defensive Rebounds">DREB</abbr></th>
-                    <th><abbr title="Rebonds">REB</abbr></th>
-                    <th><abbr title="Assists">AST</abbr></th>
-                    <th><abbr title="Steals">STL</abbr></th>
-                    <th><abbr title="Blocks">BLK</abbr></th>
-                    <th><abbr title="Turnovers">TOV</abbr></th>
-                    <th><abbr title="Points">PTS</abbr></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php
-                    echo "<td>{$player["games"]}</td>";
-                    echo "<td>{$player["MP"]}</td>";
-                    echo "<td>{$player["FG"]}</td>";
-                    echo "<td>{$player["FGA"]}</td>";
-                    echo "<td>".ltrim($player["FGP"], "0")."</td>";
-                    echo "<td>{$player["3P"]}</td>";
-                    echo "<td>{$player["3PA"]}</td>";
-                    echo "<td>".ltrim($player["3PP"], "0")."</td>";
-                    echo "<td>{$player["FT"]}</td>";
-                    echo "<td>{$player["FTA"]}</td>";
-                    echo "<td>".ltrim($player["FTP"], "0")."</td>";
-                    echo "<td>{$player["OREB"]}</td>";
-                    echo "<td>{$player["DREB"]}</td>";
-                    echo "<td>{$player["REB"]}</td>";
-                    echo "<td>{$player["AST"]}</td>";
-                    echo "<td>{$player["STL"]}</td>";
-                    echo "<td>{$player["BLK"]}</td>";
-                    echo "<td>{$player["TOV"]}</td>";
-                    echo "<td>{$player["PTS"]}</td>";
-                    ?>
-                </tr>
-            </tbody>
-        </table>
+        <header>
+            <div class="header-inner">
+                <img class="logo" src="img/nba-logo.png">
+                <div class="logo-txt">
+                    NBA PLAYER SEARCH
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Search</a></li>
+                        <li><a href="design.php">Design</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <div class="page-wrapper">
+                <?php
+                echo "<h1>{$player["first_name"]} {$player["last_name"]}</h1>";
+                foreach($player_pos as $pos){
+                    echo "<p>{$pos["name"]}</p>";
+                }
+                ?>
+                <div class="stats-tbl">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th><abbr title="Games Played">GP</abbr></th>
+                                <th><abbr title="Minuites Played">MP</abbr></th>
+                                <th><abbr title="Field Goals">FG</abbr></th>
+                                <th><abbr title="Field Goals Attempted">FGA</abbr></th>
+                                <th><abbr title="Field Goal Percentage">FG%</abbr></th>
+                                <th><abbr title="Three Point Field Goals">3P</abbr></th>
+                                <th><abbr title="Three Point Field Goal Attempts">3PA</abbr></th>
+                                <th><abbr title="Three Point Field Goal Percentage">3P%</abbr></th>
+                                <th><abbr title="Free Throws">FT</abbr></th>
+                                <th><abbr title="Free Throw Attempts">FTA</abbr></th>
+                                <th><abbr title="Free Throw Percentage">FT%</abbr></th>
+                                <th><abbr title="Offensive Rebounds">OREB</abbr></th>
+                                <th><abbr title="Defensive Rebounds">DREB</abbr></th>
+                                <th><abbr title="Rebonds">REB</abbr></th>
+                                <th><abbr title="Assists">AST</abbr></th>
+                                <th><abbr title="Steals">STL</abbr></th>
+                                <th><abbr title="Blocks">BLK</abbr></th>
+                                <th><abbr title="Turnovers">TOV</abbr></th>
+                                <th><abbr title="Points">PTS</abbr></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <?php
+                                echo "<td>{$player["games"]}</td>";
+                                echo "<td>{$player["MP"]}</td>";
+                                echo "<td>{$player["FG"]}</td>";
+                                echo "<td>{$player["FGA"]}</td>";
+                                echo "<td>".ltrim($player["FGP"], "0")."</td>";
+                                echo "<td>{$player["3P"]}</td>";
+                                echo "<td>{$player["3PA"]}</td>";
+                                echo "<td>".ltrim($player["3PP"], "0")."</td>";
+                                echo "<td>{$player["FT"]}</td>";
+                                echo "<td>{$player["FTA"]}</td>";
+                                echo "<td>".ltrim($player["FTP"], "0")."</td>";
+                                echo "<td>{$player["OREB"]}</td>";
+                                echo "<td>{$player["DREB"]}</td>";
+                                echo "<td>{$player["REB"]}</td>";
+                                echo "<td>{$player["AST"]}</td>";
+                                echo "<td>{$player["STL"]}</td>";
+                                echo "<td>{$player["BLK"]}</td>";
+                                echo "<td>{$player["TOV"]}</td>";
+                                echo "<td>{$player["PTS"]}</td>";
+                                ?>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <?php
+                    foreach($awards as $award){
+                        echo "<p>{$award["name"]}: {$award["year"]}</p>";
+                    }
+                ?>
+                </div>
+        </main>
     </body>
 </html>
