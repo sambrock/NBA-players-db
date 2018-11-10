2318252b6bad402bbdf930f97213b6a3db4df0e3
sambrock <sambrocklehurst1998@gmail.com>
2018-11-10 21:39:41 +0000

    SQL search finalised. Added multiple positions for appropriate players. Form controls stay completed once search is made.

--- index.php
+++ index.php
@@ -18,12 +18,11 @@ if(isset($_GET['p'])){
     $position=$_GET["p"];
 }
 
-$query = "SELECT players.first_name, players.last_name, players.id as player_id, teams.id as team_id, players.number, teams.abbreviation, ROUND(SUM(points / games), 1) as PTS, ROUND(SUM((offensive_rebounds + defensive_rebounds) / games),1) as REB, ROUND(SUM(assists / games), 1) as AST, ROUND(SUM(blocks / games), 1) as BLK FROM players
-INNER JOIN teams ON players.team_id=teams.id INNER JOIN player_position ON players.id=player_position.player_id INNER JOIN positions ON player_position.position_id=positions.id
+$query = "SELECT DISTINCT players.first_name, players.last_name, players.id as player_id, teams.id as team_id, players.number, teams.abbreviation, ROUND(SUM(points / games), 1) as PTS, ROUND(SUM((offensive_rebounds + defensive_rebounds) / games),1) as REB, ROUND(SUM(assists / games), 1) as AST, ROUND(SUM(blocks / games), 1) as BLK FROM players INNER JOIN player_position ON players.id=player_position.player_id INNER JOIN positions ON player_position.position_id=positions.id INNER JOIN statistics ON players.id=statistics.player_id INNER JOIN teams ON players.team_id=teams.id
 WHERE ((first_name LIKE :searchterm OR last_name LIKE :searchterm OR CONCAT(first_name,' ', last_name) LIKE :searchterm) OR :searchterm IS NULL)
 AND (teams.abbreviation = :team OR :team IS NULL)
 AND (positions.name = :position OR :position IS NULL)
-GROUP BY players.last_name";
+GROUP BY last_name, positions.name";
 $prep_stmt=$conn->prepare($query);
 $prep_stmt->bindValue(':searchterm', '%' . $searchterm . '%');
 $prep_stmt->bindValue(':team', $team);
@@ -32,12 +31,14 @@ $prep_stmt->bindValue(':position', $position);
 $prep_stmt->execute();
 $players=$prep_stmt->fetchAll();
 
-$count_stmt = "SELECT COUNT(players.last_name) as count FROM players INNER JOIN teams ON players.team_id=teams.id WHERE first_name LIKE :searchterm OR last_name LIKE :searchterm OR teams.name LIKE :searchterm";
-$count_stmt=$conn->prepare($count_stmt);
-$count_stmt->bindValue(":searchterm", $searchterm);
+$count = count($players);
 
-$count_stmt->execute();
-$count=$count_stmt->fetch();
+$pos_q = "SELECT players.id as player_id, positions.name
+FROM players INNER JOIN player_position ON players.id=player_position.player_id INNER JOIN positions ON player_position.position_id=positions.id";
+$pos_stmt=$conn->prepare($pos_q);
+
+$pos_stmt->execute();
+$positions=$pos_stmt->fetchAll();
 
 ?>
 <html lang="en">
@@ -65,11 +66,11 @@ $count=$count_stmt->fetch();
             <div class="search-container">
                 <form action="index.php" method="GET" id="search-form">
                     <div class="search-box">
-                        <input type="text" class="form-control" name="q" id="search" placeholder="Search by player name"><button type="sumbit" class="search-btn"></button>
+                        <input type="text" class="form-control" name="q" id="search" placeholder="Search by player name" value="<?php if(isset($_GET['q'])){ echo "$searchterm"; } ?>"><button type="sumbit" class="search-btn"></button>
                     </div>
                     <div class="filters">
-                        <select name="t" placeholder="Team" class="form-control">
-                            <option value="" hidden>Team</option>
+                        <select name="t" placeholder="Team" id="team-select" class="form-control" >
+                            <option value="">Team</option>
                             <option value="ATL">Atlanta Hawks</option>
                             <option value="BOS">Boston Celtics</option>
                             <option value="BKN">Brooklyn Nets</option>
@@ -95,21 +96,33 @@ $count=$count_stmt->fetch();
                             <option value="PHI">Philadelphia 76ers</option>
                             <option value="PHX">Phoenix Suns</option>
                             <option value="POR">Portland Trail Blazers</option>
+                            <option value="SAC">Sacramento Kings</option>
+                            <option value="SAS">San Antonio Spurs</option>
+                            <option value="TOR">Toronto Raptors</option>
+                            <option value="UTA">Utah Jazz</option>
+                            <option value="WAS">Washington Wizards</option>
                         </select>
-                        <select name="p" placeholder="Position" class="form-control">
-                            <option value="" hidden>Position</option>
+                        <select name="p" placeholder="Position" id="position-select" class="form-control">
+                            <option value="">Position</option>
                             <option value="G">Guard</option>
                             <option value="F">Forward</option>
                             <option value="C">Center</option>
                         </select>
+                        <script>
+                            <?php if(isset($_GET['t'])){ ?>
+                            document.getElementById('team-select').value = "<?php echo "$team";?>";
+                            <?php } if(isset($_GET['p'])){ ?>
+                            document.getElementById('position-select').value = "<?php echo "$position";?>";
+                            <?php } ?>
+                        </script>
                     </div>
                 </form>
             </div>
-            <?php if(isset($_GET['q']) || ($_GET['t']) || ($_GET['p'])){ ?>
+            <?php if(isset($_GET['q']) || isset($_GET['t']) || isset($_GET['p'])){ ?>
             <div class="results-container">
                 <div class="results">
                     <div class="results-header">
-                        <span class="results-num"><?php echo $count["count"]; ?> results</span>
+                        <span class="results-num"><?php echo $count; ?> results</span>
                         <div class="result-stat-header">
                             <span>PTS</span>
                             <span>REB</span>
@@ -118,13 +131,19 @@ $count=$count_stmt->fetch();
                         </div>
                     </div>
                     <?php
-                        foreach($players as $player){
-                            echo "<div class='result-player'>";
-                            echo "<div class='result-player-name'><a href='details.php?id={$player["player_id"]}'>{$player["first_name"]} {$player["last_name"]}</a></div>";
-                            echo "<div class='result-player-info'>#{$player["number"]} | <span></span> | {$player["abbreviation"]}</div>";
-                            echo "<div class='result-player-stats'><span class='result-stat'>{$player["PTS"]}</span><span class='result-stat'>{$player["REB"]}</span><span class='result-stat'>{$player["AST"]}</span><span class='result-stat'>{$player["BLK"]}</span></div>";
-                            echo "</div>";
-                        }
+                      foreach($players as $player){
+                          echo "<div class='result-player'>";
+                          echo "<div class='result-player-name'><a href='details.php?id={$player["player_id"]}'>{$player["first_name"]} {$player["last_name"]}</a></div>";
+                          echo "<div class='result-player-info'>#{$player["number"]} | ";
+                          foreach($positions as $pos){
+                             if($pos["player_id"] == $player["player_id"]){
+                                 echo "<span>{$pos["name"]}</span>";
+                             }
+                          }
+                          echo " | {$player["abbreviation"]}</div>";
+                          echo "<div class='result-player-stats'><span class='result-stat'>{$player["PTS"]}</span><span class='result-stat'>{$player["REB"]}</span><span class='result-stat'>{$player["AST"]}</span><span class='result-stat'>{$player["BLK"]}</span></div>";
+                          echo "</div>";
+                      }
                     ?>
                 </div>
             </div>
